147b1fef248673a624f7777b390bc49c56ad6e67
TejaReddy7939 <tejaswinipadmachinthakunta@gmail.com>
2026-06-21 11:41:12 +0530

    Fix register image

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import axios from "axios";
 import "./Register.css";
 import { Link } from "react-router-dom";
-
+import libraryImage from "../assets/Screenshot 2026-06-21 112916.png";
 const API_URL = import.meta.env.VITE_API_URL;
 
 
@@ -87,7 +87,7 @@ function Register() {
       </div>
       <div className="image-panel">
         <img
-          src="\Gemini_Generated_Image_ughiaeughiaeughi.png"
+          src={libraryImage}
           alt="Aesthetic Bright Library Loft"
         />
       </div>
